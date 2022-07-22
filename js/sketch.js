791df73f13f26ebb96bad470609559441b514cc9
wty262690 <71436111+wty262690@users.noreply.github.com>
2022-07-22 22:12:03 +0800

    p5 video

--- js/sketch.js
+++ js/sketch.js
@@ -3,36 +3,41 @@ let fingers;
 let button;
 
 function setup() {
-    var canvas = createCanvas(min(windowWidth,600), 400);
+    var canvas = createCanvas(min(windowWidth,600), min(windowWidth,400));
     canvas.parent('canvasForHTML');
-    fingers = createVideo("/js/readbook2.mov");
-    fingers.size(min(windowWidth,600), 400);
-    fingers.loop();
-    fingers.hide();
+    readbook = createVideo("/js/video/readbook.mp4");
+    headup = createVideo("/js/video/headup.mp4");
+    headdown = createVideo("/js/video/headdown.mp4");
+    readbook.loop();
     button = createButton('play');
     button.mousePressed(toggleVid); // attach button listener
+    readbook.hide();
+    headup.hide();
+    headdown.hide();
+    vid=readbook;
 }
 
 function draw() {
-    background(127);
-    let img = fingers.get();
-    image(img, 0, 0);
+  background(0);
+  image(vid,0,0,width,height);
 }
 
-// plays or pauses the video depending on current state
+
+function windowResized(){
+
+}
+
 function toggleVid() {
   if (playing) {
-    fingers.pause();
+    vid.pause();
+    vid=headup;
+    headup.play();
     button.html('play');
   } else {
-    fingers.loop();
+    vid.pause();
+    vid=readbook;
+    readbook.play();
     button.html('pause');
   }
   playing = !playing;
-}
-
-function windowResized(){
-  canvas = createCanvas(max(10,min(windowWidth,600)), max(10,min(400,windowWidth/2)));
-  canvas.parent('canvasForHTML');
-
 }
