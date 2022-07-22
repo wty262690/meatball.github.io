3dec933589123357a7483791ede024704cde4179
wty262690 <71436111+wty262690@users.noreply.github.com>
2022-07-22 22:59:02 +0800

    p5 video

--- js/sketch.js
+++ js/sketch.js
@@ -1,20 +1,25 @@
-let playing = false;
-let fingers;
+let playing = true;
+let vid,readbook,headup,headdown;
 let button;
 
 function setup() {
-    var canvas = createCanvas(min(windowWidth,600), min(windowWidth,400));
-    canvas.parent('canvasForHTML');
+    setcanvas();
     readbook = createVideo("/js/video/readbook.mp4");
     headup = createVideo("/js/video/headup.mp4");
     headdown = createVideo("/js/video/headdown.mp4");
-    readbook.loop();
-    button = createButton('play');
-    button.mousePressed(toggleVid); // attach button listener
+    //button = createButton('play');
+    //button.mousePressed(toggleVid); // attach button listener
     readbook.hide();
     headup.hide();
     headdown.hide();
     vid=readbook;
+}
+
+function afterLoad() { 
+  text("The video has finished loading and will"+ " now play!", 20, 40); 
+  readbook.loop();
+  vid=readbook;
+  image(vid,0,0,width,height);
 }
 
 function draw() {
@@ -24,20 +29,31 @@ function draw() {
 
 
 function windowResized(){
-
+  setcanvas();
 }
 
-function toggleVid() {
+function mouseClicked() {
   if (playing) {
     vid.pause();
     vid=headup;
     headup.play();
-    button.html('play');
+    //button.html('play');
   } else {
     vid.pause();
     vid=readbook;
-    readbook.play();
-    button.html('pause');
+    readbook.loop();
   }
   playing = !playing;
+}
+
+function setcanvas(){
+  var canvas;
+  if (windowWidth*(40/100)>=400){
+    canvas = createCanvas(min(windowWidth*(40/100),600), min((windowWidth*(40/100)/1920)*1080,600/1920*1080));
+  }
+  else{
+    canvas = createCanvas(min(windowWidth*(80/100),600), min((windowWidth*(80/100)/1920)*1080,600/1920*1080));
+  }
+  canvas.parent('canvasForHTML');
+  
 }
