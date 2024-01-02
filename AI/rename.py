71ac589d22df13fa1b3a3e83fde59f1c70cdaa4c
Wu tung yu <wty262690@gmail.com>
2024-01-02 10:34:37 +0800

    update describe

--- AI/rename.py
+++ AI/rename.py
@@ -1,7 +1,7 @@
 import os
 
 # folder path
-dir_path = r'D:/桌面/creat/程式碼/Meatball-Page/wty262690.github.io/AI/image/imageCameraImage2/'
+dir_path = r'D://桌面/功課/大肆上/都人工的/AI_HW2/imageCameraImage2'
 
 # list to store files
 res = []
@@ -11,9 +11,9 @@ for path in os.listdir(dir_path):
     # check if current path is a file
     if os.path.isfile(os.path.join(dir_path, path)):
         i += 1
-        renampath = path.split('mm')
+        renampath = path.split('.')
         print(renampath[0])
-        os. rename(os.path.join(dir_path, path), os.path.join(dir_path, str(renampath[0]) + '.jpg')) 
+        os. rename(os.path.join(dir_path, path), os.path.join(dir_path, str(renampath[0]) + '.png')) 
         #res.append(renampath[0] + '.png')
 #print(res)
 #os. rename()
